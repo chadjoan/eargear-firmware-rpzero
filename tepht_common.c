ed455fe429a79d3fac8dbd0d7baab66f5e337eeb
Chad Joan <chadjoan@gmail.com>
2022-11-26 08:43:33 -0500

    Squashed 'depends/drivers/te-sensor-drivers-pht/' changes from 9c5f2605..7fabefb3
    7fabefb3 Merge the *_host_functions structure into tepht_common.*
    b85f5dd7 Bugfix + better testing for `tepht_int64_to_string`
    c2807f3b Eliminate warnings from `-Wunused-parameter`
    51e5733d Add some commented-out error handling possiblities.
    
    git-subtree-dir: depends/drivers/te-sensor-drivers-pht
    git-subtree-split: 7fabefb3d0738e9ecaf9004296c4a98f428f0819

--- tepht_common.c
+++ tepht_common.c
@@ -46,7 +46,7 @@ static char *tepht_uint64_to_string_impl(uint64_t num, uint8_t base, char *buffe
 	// Write the number out.
 	// This will put it out in reverse order, so we'll have to reverse it later.
 	size_t digitStart = i;
-	while(num > 0)
+	while(num > 0 && i < (buflen-1))
 	{
 		uint8_t digit = (uint8_t)(num % base);
 		num /= base;
@@ -118,6 +118,7 @@ char *tepht_int64_to_string(int64_t num, uint8_t base, char *buffer, size_t bufl
 	assert( 2 <= base && base <= 36 );
 
 	assert(buffer != NULL);
+	assert(buflen >= 65 || base !=  2); // 1 sign + 63 bits + 1 null terminator
 	assert(buflen >= 21 || base != 10); // 1 sign + 19 digits + 1 null terminator
 	assert(buflen >= 18 || base != 16); // 1 sign + 16 hexadigits + 1 null terminator
 
@@ -181,6 +182,7 @@ char *tepht_uint64_to_string(uint64_t num, uint8_t base, char *buffer, size_t bu
 	assert( 2 <= base && base <= 36 );
 
 	assert(buffer != NULL);
+	assert(buflen >= 65 || base !=  2); // 64 bits + 1 null terminator
 	assert(buflen >= 21 || base != 10); // 20 digits + 1 null terminator
 	assert(buflen >= 17 || base != 16); // 16 hexadigits + 1 null terminator
 
@@ -237,8 +239,9 @@ char *tepht_uint64_to_hex(uint64_t num, char *buffer, size_t buflen, size_t *out
 
 static void unittest_int_to_str(tepht_string_printer  *printer)
 {
-	char buffer[32];
+	char buffer[72];
 	size_t buflen = sizeof(buffer);
+	size_t outlen = 0;
 
 	tepht_print_array_string(printer, "  unittest_int_to_str, for the tepht_int64_to_string(...) function:");
 
@@ -249,26 +252,42 @@ static void unittest_int_to_str(tepht_string_printer  *printer)
 	assert(0 == strcmp(tepht_int64_to_dec(-8 , buffer, buflen, NULL), "-8" ));
 	assert(0 == strcmp(tepht_int64_to_dec(-32, buffer, buflen, NULL), "-32"));
 
-	assert(0 == strcmp(tepht_int64_to_hex(0  , buffer, buflen, NULL), "0"  ));
-	assert(0 == strcmp(tepht_int64_to_hex(-0 , buffer, buflen, NULL), "0"  ));
-	assert(0 == strcmp(tepht_int64_to_hex(32 , buffer, buflen, NULL), "20" ));
-	assert(0 == strcmp(tepht_int64_to_hex(31 , buffer, buflen, NULL), "1F" ));
-	assert(0 == strcmp(tepht_int64_to_hex(-8 , buffer, buflen, NULL), "-8" ));
-	assert(0 == strcmp(tepht_int64_to_hex(-32, buffer, buflen, NULL), "-20"));
+	assert(0 == strcmp(tepht_int64_to_dec(0  , buffer, buflen, &outlen), "0"  )); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_dec(-0 , buffer, buflen, &outlen), "0"  )); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_dec(32 , buffer, buflen, &outlen), "32" )); assert(outlen == 2);
+	assert(0 == strcmp(tepht_int64_to_dec(31 , buffer, buflen, &outlen), "31" )); assert(outlen == 2);
+	assert(0 == strcmp(tepht_int64_to_dec(-8 , buffer, buflen, &outlen), "-8" )); assert(outlen == 2);
+	assert(0 == strcmp(tepht_int64_to_dec(-32, buffer, buflen, &outlen), "-32")); assert(outlen == 3);
 
-	assert(0 == strcmp(tepht_int64_to_hex(10, buffer, buflen, NULL), "A"));
-	assert(0 == strcmp(tepht_int64_to_hex(11, buffer, buflen, NULL), "B"));
-	assert(0 == strcmp(tepht_int64_to_hex(12, buffer, buflen, NULL), "C"));
-	assert(0 == strcmp(tepht_int64_to_hex(13, buffer, buflen, NULL), "D"));
-	assert(0 == strcmp(tepht_int64_to_hex(14, buffer, buflen, NULL), "E"));
-	assert(0 == strcmp(tepht_int64_to_hex(15, buffer, buflen, NULL), "F"));
+	assert(0 == strcmp(tepht_int64_to_hex(0  , buffer, buflen, &outlen), "0"  )); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_hex(-0 , buffer, buflen, &outlen), "0"  )); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_hex(32 , buffer, buflen, &outlen), "20" )); assert(outlen == 2);
+	assert(0 == strcmp(tepht_int64_to_hex(31 , buffer, buflen, &outlen), "1F" )); assert(outlen == 2);
+	assert(0 == strcmp(tepht_int64_to_hex(-8 , buffer, buflen, &outlen), "-8" )); assert(outlen == 2);
+	assert(0 == strcmp(tepht_int64_to_hex(-32, buffer, buflen, &outlen), "-20")); assert(outlen == 3);
 
-	assert(0 == strcmp(tepht_int64_to_binary(0  , buffer, buflen, NULL), "0"      ));
-	assert(0 == strcmp(tepht_int64_to_binary(-0 , buffer, buflen, NULL), "0"      ));
-	assert(0 == strcmp(tepht_int64_to_binary(32 , buffer, buflen, NULL), "100000" ));
-	assert(0 == strcmp(tepht_int64_to_binary(31 , buffer, buflen, NULL), "11111"  ));
-	assert(0 == strcmp(tepht_int64_to_binary(-8 , buffer, buflen, NULL), "-1000"  ));
-	assert(0 == strcmp(tepht_int64_to_binary(-32, buffer, buflen, NULL), "-100000"));
+	assert(0 == strcmp(tepht_int64_to_hex(10, buffer, buflen, &outlen), "A")); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_hex(11, buffer, buflen, &outlen), "B")); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_hex(12, buffer, buflen, &outlen), "C")); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_hex(13, buffer, buflen, &outlen), "D")); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_hex(14, buffer, buflen, &outlen), "E")); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_hex(15, buffer, buflen, &outlen), "F")); assert(outlen == 1);
+
+	assert(0 == strcmp(tepht_int64_to_binary(0  , buffer, buflen, &outlen), "0"      )); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_binary(-0 , buffer, buflen, &outlen), "0"      )); assert(outlen == 1);
+	assert(0 == strcmp(tepht_int64_to_binary(32 , buffer, buflen, &outlen), "100000" )); assert(outlen == 6);
+	assert(0 == strcmp(tepht_int64_to_binary(31 , buffer, buflen, &outlen), "11111"  )); assert(outlen == 5);
+	assert(0 == strcmp(tepht_int64_to_binary(-8 , buffer, buflen, &outlen), "-1000"  )); assert(outlen == 5);
+	assert(0 == strcmp(tepht_int64_to_binary(-32, buffer, buflen, &outlen), "-100000")); assert(outlen == 7);
+
+	// Test what happens if we force an overflow/truncation.
+	char buffer3[3];
+	const char *outstr = NULL;
+	outstr = tepht_uint64_to_string_impl(123, 10, buffer3, 3, &outlen);
+	assert(outlen == 2);
+	assert(outstr[0] == '2');
+	assert(outstr[1] == '3');
+	assert(outstr[2] == '\0');
 
 	tepht_print_array_string(printer, "  Passed.\n");
 }
@@ -315,6 +334,65 @@ size_t tepht_print_uint64_as_dec(tepht_string_printer  *printer, uint64_t number
 	printer->print_string(printer->printer_context, str, len);
 	return len;
 }
+
+#if 0
+// This could be a powerful way to store file/line/function info in
+// a compiler-and-platform-independent way, but right-sizing the arrays
+// is too tricky for now. 
+static const char *tepht_filenames[64];
+static const char *tepht_function_names[196];
+
+#if 0
+typedef struct tepht_debug_frame {
+	uint16_t  word1;
+	uint16_t  word2;
+	uint16_t  word3;
+} tepht_debug_frame;
+
+static tepht_debug_frame  tepht_debug_frame[TEPHT_DEBUG_FRAME_DB_ROW_CAPACITY];
+
+#define tepht_dframe_to_u64(dframe) ( \
+	((uint64_t)(dframe.word1) <<  0) | \
+	((uint64_t)(dframe.word2) << 16) | \
+	((uint64_t)(dframe.word3) << 32) )
+#endif
+
+static uint16_t  tepht_debug_frame_db[TEPHT_DEBUG_FRAME_DB_ROW_CAPACITY*3];
+
+// Below we have a bitfield layout that supports:
+//
+// Files with up to (2^18 = 262144) lines.
+// Function count (project-wide) up to (2^18 = 262144).
+// File count up to (2^12 = 4096).
+//
+// And so, as long as the project stays within those limits, we can save some
+// memory and store every line of debug information with about 48 bits each
+// (not including the string pointers referenced by these handles --
+// which is a separate problem).
+//
+
+#define tepht_dframe_to_u64(d0,d1,d2) ( \
+	((uint64_t)(d0) <<  0) | \
+	((uint64_t)(d1) << 16) | \
+	((uint64_t)(d2) << 32) )
+
+#define tepht_dframe_get_line_number(d0,d1,d2)    ((tepht_dframe_to_u64(d0,d1,d2) & 0x00000003FFFFu) >>  0)
+#define tepht_dframe_get_function_handle(d0,d1,d2) ((tepht_dframe_to_u64(d0,d1,d2) & 0x000FFFFC0000u) >> 18)
+#define tepht_dframe_get_filename_handle(d0,d1,d2) ((tepht_dframe_to_u64(d0,d1,d2) & 0xFFF000000000u) >> 36)
+
+size_t  tepht_upsert_debug_filename(const char *filename)
+{
+	// Ugh datastructures.
+	// Need a sublinear find-and-insert-as-needed algorithm.
+}
+
+tepht_dframe_handle  tepht_upsert_debug_location_to_dframe(const char *filename, const char *function_name, size_t  line_number)
+{
+	
+}
+
+#endif
+
 
 // This table exists as an optimization.
 // It allows us to return filename information as an 8-bit index into this
@@ -760,6 +838,162 @@ static void unittest_print_error(tepht_string_printer  *printer)
 	tepht_print_array_string(printer, "  Passed.\n");
 }
 
+
+static tepht_status_u16
+	i2c_controller_read_unimpl(void *caller_context, tepht_i2c_controller_packet *const packet)
+{
+	(void)caller_context;
+	(void)packet;
+	return tepht_status_i2c_read_unimplemented;
+}
+
+static tepht_status_u16
+	i2c_controller_write_unimpl(void *caller_context, tepht_i2c_controller_packet *const packet)
+{
+	(void)caller_context;
+	(void)packet;
+	return tepht_status_i2c_write_unimplemented;
+}
+
+static tepht_status_u16
+	i2c_controller_write_no_stop_unimpl(void *caller_context, tepht_i2c_controller_packet *const packet)
+{
+	(void)caller_context;
+	(void)packet;
+	return tepht_status_i2c_write_no_stop_unimplemented;
+}
+
+static tepht_status_u16
+	sleep_ms_unimpl(void *caller_context, uint32_t milliseconds)
+{
+	(void)caller_context;
+	(void)milliseconds;
+	return tepht_status_sleep_ms_unimplemented;
+}
+
+static void print_string_stub(void *caller_context, const char *text)
+{
+	(void)caller_context;
+	(void)text;
+}
+
+static void print_int64_stub(void *caller_context,  int64_t number, uint8_t pad_width,  tepht_bool  pad_with_zeroes)
+{
+	(void)caller_context;
+	(void)number;
+	(void)pad_width;
+	(void)pad_with_zeroes;
+}
+
+/// \brief Initializes the `ms8607_host_functions` struct; this should be called
+///        *before* assigning function pointers into the structure.
+///
+static tepht_error_info  tepht_init_host_functions(tepht_host_functions *deps)
+{
+	if ( deps == NULL )
+		return tepht_error(NULL, tepht_status_null_argument);
+
+	deps->validated_ = 0;
+
+	deps->i2c_controller_read           = &i2c_controller_read_unimpl;
+	deps->i2c_controller_write          = &i2c_controller_write_unimpl;
+	deps->i2c_controller_write_no_stop  = &i2c_controller_write_no_stop_unimpl;
+	deps->sleep_ms                      = &sleep_ms_unimpl;
+	deps->print_string                  = &print_string_stub;
+	deps->print_int64                   = &print_int64_stub;
+
+	return tepht_success(NULL);
+}
+
+tepht_status  tepht_validate_mandatory_depends(tepht_host_functions *deps)
+{
+	assert(deps != NULL);
+
+	if ( deps->validated_ )
+		return tepht_status_ok;
+
+	if ( deps->i2c_controller_read == NULL
+	||   deps->i2c_controller_read == &i2c_controller_read_unimpl )
+		return tepht_status_i2c_read_unimplemented;
+	else
+	if ( deps->i2c_controller_write == NULL
+	||   deps->i2c_controller_write == &i2c_controller_write_unimpl )
+		return tepht_status_i2c_write_unimplemented;
+	/*
+	// It's actually OK to be missing the `i2c_controller_write_no_stop` function.
+	// The write-no-stop function is only needed for hold-mode ADC.
+	// So we shouldn't validate this one until the caller actually tries to
+	// use the hold-mode feature. In most cases, the caller won't need this,
+	// and in some of those cases, the caller *can't* provide this (ex:
+	// because their MCU's I2C peripheral doesn't allow sending a transmit
+	// sequence without the trailing `stop` bit).
+	// TODO: Maybe a -D macro could be provided to enable early checking of this?
+	// TODO: Sort of like USE-flags on Gentoo systems... USE_HOLD implies RDEPENDS={$RDEPENDS, write-no-stop}
+	else
+	if ( deps->i2c_controller_write_no_stop == NULL
+	||   deps->i2c_controller_write_no_stop == &i2c_controller_write_no_stop_unimpl )
+		return tepht_status_i2c_write_no_stop_unimplemented;
+	*/
+	else
+	if ( deps->sleep_ms == NULL
+	||   deps->sleep_ms == &sleep_ms_unimpl )
+		return tepht_status_sleep_ms_unimplemented;
+
+	// Under no condition should any of the function pointers be NULL.
+	// Even unimplemented things should be assigned error handlers or stubs
+	// by the `ms8607_init_host_functions` function.
+	if( deps->i2c_controller_read == NULL
+	||  deps->i2c_controller_write == NULL
+	||  deps->i2c_controller_write_no_stop == NULL
+	||  deps->sleep_ms == NULL
+	||  deps->print_string == NULL
+	||  deps->print_int64 == NULL )
+		return tepht_status_null_host_function;
+
+	// If we've made it to the end of this function, then it is at least plausible
+	// that the driver can use this host functions object to do *something*.
+	// (More specific features of the driver might require functions that we
+	// didn't check here. However, absent a more complicated configuration system,
+	// we'll just have to check those later, at point-of-use.)
+	deps->validated_ = 1;
+	return tepht_status_ok;
+}
+
+//
+// TODO: Finish rewriting docs; might need to move some (or all) validation
+//       into the `*_init_sensor` functions. (Probably not all of it.
+//       library-wide universal dependencies should be checked as early
+//       as possible!)
+tepht_error_info  tepht_init_and_assign_host_functions(
+	tepht_host_functions *dependencies,
+	void *caller_context,
+	void (*assign_functions)(tepht_host_functions *dependencies, void *caller_context)
+	)
+{
+	tepht_error_info  einfo;
+
+	// The call to `tepht_init_host_functions` will enforce that `dependencies` is non-NULL.
+	einfo = tepht_init_host_functions(dependencies);
+	if ( tepht_is_error(einfo) )
+		return einfo;
+
+	// It's OK if `caller_context` is NULL.
+	// Whether that's required to be non-NULL or not is up to the caller, so
+	// they would have to enforce that from within `assign_functions`, if they
+	// wanted such a thing.
+	// (`caller_context` being NULL is actually pretty likely, in this case!
+	// The host functions are likely to be known at compile-time, so it
+	// would be unnecessary to use the dynamic (run-time) information
+	// referenced by the `caller_context` object to compute their values.)
+
+	assign_functions(dependencies, caller_context);
+
+	tepht_status  status = tepht_validate_mandatory_depends(dependencies);
+	if ( status != tepht_status_ok )
+		return tepht_error(NULL, status);
+
+	return tepht_success(NULL);
+}
 
 tepht_bool  tepht_pt_sensor_is_connected(tepht_pt_sensor  sensor,  void *caller_context)
 {
